5878a0252877620cfabbd330394a6bda0044ec8e
Codetesting <37256601+Codetesing@users.noreply.github.com>
2021-12-01 16:43:50 +0900

    application
    application

---
+++ application/PersonalData.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+public class PersonalData {
+
+    private String member_mid;
+
+    public String getMember_mid(){
+        return member_mid;
+    }
+
+    public void setMember_mid(String member_mid) {
+        this.member_mid = member_mid;
+    }
+}
